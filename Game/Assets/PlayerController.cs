29f8c778c98dd44591b120455a39a2c49effd748
Nicholas Anderson <Nicholas.D.Anderson.0@gmail.com>
2017-10-15 15:50:05 -0700

    Fix some camera movement

--- Game/Assets/PlayerController.cs
+++ Game/Assets/PlayerController.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float boostMultiplier;
+    public GameObject cursorTarget;
     public float speed = 10.0F;
     public float zoomSpeed = 10.0F;
     public float zoomSmooth = 10.0F;
@@ -64,51 +64,24 @@ public class PlayerController : MonoBehaviour
                 Debug.Log("Blam");
                 break;
         }
-        
-        float speedMod = Input.GetKey(KeyCode.LeftShift) ? speed * boostMultiplier : speed;
-        float x = Input.GetAxis("Vertical") * speedMod;
-        float y = Input.GetAxis("Horizontal") * speedMod;
+        */
+        float x = Input.GetAxis("Vertical");
+        float y = Input.GetAxis("Horizontal");
         x *= Time.deltaTime;
         y *= Time.deltaTime;
         Vector3 shiftMove = new Vector3(y, 0, x);
-        */
+
+        Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+
+        if (Physics.Raycast(ray, out hit, 1000))
+        {
+            cursorTarget.transform.position = hit.point;
+        }
+
         zoom -= Input.GetAxis("Mouse ScrollWheel") * zoomSpeed;
 
         cam.orthographicSize = Mathf.Lerp(cam.orthographicSize, zoom, Time.deltaTime * zoomSpeed);
-
-        if (Input.GetMouseButtonDown(0))
-        {
-            //dragOrigin = Input.mousePosition;
-            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
-
-            if (Physics.Raycast(ray, out hit, 100000, dragMask))
-            {
-                onClickPoint = hit.point;
-                Debug.Log(hit.collider.name);
-            }
-        }
-
-        if (Input.GetMouseButton(0))
-        {
-
-        }
-        //Vector3 pos = Camera.main.ScreenToViewportPoint(Input.mousePosition - dragOrigin);
-        //Vector3 move = new Vector3(pos.x * dragSpeed, 0, pos.y * dragSpeed);
-
        
-        //transform.Translate(shiftMove, Space.World);
+        transform.Translate(shiftMove, Space.World);
     }
-}
- 
-public class CameraDrag : MonoBehaviour
-{
-
-
-
-    void Update()
-    {
-
-    }
-
-
 }
